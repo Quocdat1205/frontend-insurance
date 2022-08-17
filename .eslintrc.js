fcecd32586101d7831d49990037be7713747861b
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-08-17 18:10:16 +0700

    setup - eslint  - spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,76 +1,76 @@
 module.exports = {
-  extends: [
-    'airbnb-base',
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
-    'airbnb/hooks',
-  ],
-  plugins: ['prettier', 'import'],
-  rules: {
-    // 'prettier/prettier': ['error', { endOfLine: 'auto', },],
-    'prettier/prettier': ['warn', { endOfLine: 'auto', singleQuote: true }],
-    'import/no-unresolved': 'error', // turn on errors for missing imports
-    'react/jsx-filename-extension': [0, { extensions: ['.tsx'] }],
-    camelcase: 'off',
-    'import/no-cycle': 'off',
-    'default-param-last': 'off',
-    'no-return-await': 'off',
-    'import/prefer-default-export': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-underscore-dangle': 'off',
-    'quote-props': 'off',
-    radix: 'off',
-    'class-methods-use-this': 'off',
-    'interface-name-prefix': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    'no-use-before-define': 'off',
-    'no-unused-expressions': 'off',
-    'react/jsx-props-no-spreading': [
-      0,
-      { html: 'ignore', custom: 'ignore', explicitSpread: 'ignore' },
+    extends: [
+        'airbnb-base',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb/hooks',
     ],
-    'import/order': [
-      2,
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
+    plugins: ['prettier', 'import'],
+    rules: {
+        // 'prettier/prettier': ['error', { endOfLine: 'auto', },],
+        'prettier/prettier': ['warn', { endOfLine: 'auto', singleQuote: true }],
+        'import/no-unresolved': 'error', // turn on errors for missing imports
+        'react/jsx-filename-extension': [0, { extensions: ['.tsx'] }],
+        camelcase: 'off',
+        'import/no-cycle': 'off',
+        'default-param-last': 'off',
+        'no-return-await': 'off',
+        'import/prefer-default-export': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-underscore-dangle': 'off',
+        'quote-props': 'off',
+        radix: 'off',
+        'class-methods-use-this': 'off',
+        'interface-name-prefix': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        'no-use-before-define': 'off',
+        'no-unused-expressions': 'off',
+        'react/jsx-props-no-spreading': [
+            0,
+            { html: 'ignore', custom: 'ignore', explicitSpread: 'ignore' },
         ],
-        'newlines-between': 'ignore',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: false,
-        },
-      },
-    ],
-    'import/newline-after-import': 1,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-  },
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+        'import/order': [
+            2,
+            {
+                groups: [
+                    'builtin',
+                    'external',
+                    'internal',
+                    'parent',
+                    'sibling',
+                    'index',
+                ],
+                'newlines-between': 'ignore',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: false,
+                },
+            },
+        ],
+        'import/newline-after-import': 1,
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
     },
-    'import/resolver': {
-      typescript: {
+    parserOptions: {
         project: './tsconfig.eslint.json',
-      },
     },
-  },
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.eslint.json',
+            },
+        },
+    },
 }
--- next.config.js
+++ next.config.js
@@ -1,32 +1,34 @@
 // /** @type {import('next').NextConfig} */
 const path = require('path')
+
+const { ANALYZE } = process.env
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: ANALYZE === 'true',
+    enabled: ANALYZE === 'true',
 })
 const withPlugins = require('next-compose-plugins')
 const withFonts = require('next-fonts')
 
-const { ANALYZE } = process.env
 const { i18n } = require('./next-i18next.config')
 
 const nextConfig = withPlugins([[withBundleAnalyzer], [withFonts]], {
-  reactStrictMode: false,
-  i18n,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  // images: {
-  //   domains: [
-  //     'test.nami.exchange',
-  //     'static.namifutures.com',
-  //     'sgp1.digitaloceanspaces.com',
-  //     'nami.io',
-  //     'datav2.nami.exchange',
-  //   ],
-  // },
-  distDir: process.env.BUILD_DIR || 'build',
+    reactStrictMode: false,
+    i18n,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    // images: {
+    //   domains: [
+    //     'test.nami.exchange',
+    //     'static.namifutures.com',
+    //     'sgp1.digitaloceanspaces.com',
+    //     'nami.io',
+    //     'datav2.nami.exchange',
+    //   ],
+    // },
+    distDir: process.env.BUILD_DIR || 'build',
 })
 module.exports = nextConfig
