bc156bac263bb532c079fe5cda35b9496ac6e326
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-09-27 14:02:57 +0700

    Insurance - dev - config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'nami-insurance-web',
-            script: 'yarn start',
+            script: 'yarn start:dev',
             max_memory_restart: '2G',
             env: {
                 NODE_ENV: 'development',
