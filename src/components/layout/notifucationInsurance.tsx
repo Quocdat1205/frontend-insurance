c84897a9492d2decc3f5b82426390f08182c81a1
Thuc <thucnhg@nami.trade>
2022-09-27 10:00:16 +0700

    update

--- src/components/layout/notifucationInsurance.tsx
+++ src/components/layout/notifucationInsurance.tsx
@@ -1,12 +1,13 @@
 import Button from 'components/common/Button/Button'
 import { Input } from 'components/common/Input/input'
-import { Loading, SuccessIcon } from 'components/common/Svg/SvgIcon'
+import { SuccessIcon } from 'components/common/Svg/SvgIcon'
 import Config from 'config/config'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { API_SUBSCRIBE } from 'services/apis'
 import fetchApi from 'services/fetch-api'
+import styled from 'styled-components'
 import { formatNumber } from 'utils/utils'
 
 export type iProps = {
@@ -52,7 +53,13 @@ const NotificationInsurance = ({ id, name, state, active, setActive, isMobile }:
         },
         {
             name: 'loading',
-            icon: <Loading />,
+            icon: (
+                <div className="flex flex-col space-y-6 justify-center items-center">
+                    <Loading>
+                        <div className="bg-white w-[calc(5rem-30px)] h-[calc(5rem-30px)] sm:w-[calc(7rem-40px)] sm:h-[calc(7rem-40px)] flex items-center justify-center rounded-full" />
+                    </Loading>
+                </div>
+            ),
             description: `${t('insurance:final:loading')}`,
             reason: ``,
         },
@@ -98,7 +105,7 @@ const NotificationInsurance = ({ id, name, state, active, setActive, isMobile }:
                         if (item.name === name) {
                             return (
                                 <div key={index} className={`${index != 3 && 'divide-[#E5E7E8] divide-y '} bg-white text-sm  w-full mx-auto `}>
-                                    <div className="flex flex-col justify-center items-center my-[24px] text-xl font-medium">
+                                    <div className="flex flex-col justify-center items-center mb-[24px] text-xl font-medium">
                                         {noti[index].icon}
                                         <div className="text-center w-[70%] pt-[24px]">
                                             {noti[index].description}{' '}
@@ -110,14 +117,14 @@ const NotificationInsurance = ({ id, name, state, active, setActive, isMobile }:
                                     <div className="flex flex-col justify-center mx-[24px] ">
                                         {index < 3 && (
                                             <>
-                                                <div className="flex flex-row justify-between py-[16px] px-[8px] text-base">
+                                                <div className="flex flex-row justify-between pb-[0.5rem] text-base">
                                                     <div className={'text-[#808890]'}>Q-Claim</div>
                                                     <div className={'font-semibold flex flex-row hover:cursor-pointer'}>
                                                         <span className="mr-[8px]">{Number(formatNumber(state?.q_claim, 2))}</span>
                                                         <span className={''}>USDT</span>
                                                     </div>
                                                 </div>
-                                                <div className="flex flex-row justify-between py-[16px] px-[8px] ">
+                                                <div className="flex flex-row justify-between pt-[0.5rem]">
                                                     <div className={'text-[#808890]'}>R-Claim</div>
                                                     <div className={'font-semibold'}>
                                                         <span className="mr-[8px]">{Number(formatNumber(state?.r_claim, 2))}</span>
@@ -194,14 +201,14 @@ const NotificationInsurance = ({ id, name, state, active, setActive, isMobile }:
                                     <div className="flex flex-col justify-center mx-[24px] border-t border-1 border-[#E5E7E8]">
                                         {index < 3 && (
                                             <>
-                                                <div className="flex flex-row justify-between py-[16px] px-[8px] font-base">
+                                                <div className="flex flex-row justify-between pt-[1rem] pb-[0.5rem] px-[0.5rem] font-base">
                                                     <div className={'text-[#808890]'}>Q-Claim</div>
                                                     <div className={'font-semibold flex flex-row hover:cursor-pointer'}>
                                                         <span className="mr-[8px]">{Number(formatNumber(state?.q_claim, 2))}</span>
                                                         <span className={''}>USDT</span>
                                                     </div>
                                                 </div>
-                                                <div className="flex flex-row justify-between py-[16px] px-[8px] ">
+                                                <div className="flex flex-row justify-between  pb-[1rem] px-[0.5rem] ">
                                                     <div className={'text-[#808890]'}>R-Claim</div>
                                                     <div className={'font-semibold'}>
                                                         <span className="mr-[8px]">{Number(formatNumber(state?.r_claim, 2))}</span>
@@ -264,5 +271,20 @@ const NotificationInsurance = ({ id, name, state, active, setActive, isMobile }:
         <></>
     )
 }
+
+const Loading = styled.div.attrs({
+    className:
+        'gradient-spin after:!w-[15px] after:!h-[15px] sm:after:!w-5 sm:after:!h-5 w-[5rem] h-[5rem] sm:w-[7rem] sm:h-[7rem] animate-spin-reverse flex items-center justify-center rounded-full relative',
+})`
+    &:after {
+        content: '';
+        position: absolute;
+        background: #eb2b3e;
+        width: 20px;
+        height: 20px;
+        bottom: 0;
+        border-radius: 50%;
+    }
+`
 
 export default NotificationInsurance
