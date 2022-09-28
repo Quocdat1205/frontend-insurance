45889320633459a1d907fca0f853d7ec652587d3
Quocdat1205 <quocdatsc1205@gmail.com>
2022-09-28 10:23:36 +0700

    fix config sc address

--- src/components/web3/constants/contractAddress.tsx
+++ src/components/web3/constants/contractAddress.tsx
@@ -4,6 +4,11 @@
 // export const ETHaddress = '0x2170Ed0880ac9A755fd29B2688956BD959F933F8'
 
 //test on dev
-export const USDTaddress = '0x8DADB191832eAD8D7680d6f38937b32438b0FAe4'
+// export const USDTaddress = '0x8DADB191832eAD8D7680d6f38937b32438b0FAe4'
+// export const ETHaddress = '0x8BaBbB98678facC7342735486C851ABD7A0d17Ca'
+// export const contractAddress = '0x74440DCDF863d23841ABA60eafafb0F43d00219D'
+
+// testnet
+export const USDTaddress = '0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684'
 export const ETHaddress = '0x8BaBbB98678facC7342735486C851ABD7A0d17Ca'
-export const contractAddress = '0x74440DCDF863d23841ABA60eafafb0F43d00219D'
+export const contractAddress = '0x8DADB191832eAD8D7680d6f38937b32438b0FAe4'
