b21fdab961ba5416dc95b3a9fe71ebe003541b10
nhatlong1303 <53816049+nhatlong1303@users.noreply.github.com>
2022-10-03 15:31:17 +0700

    Insurance - Head - fix:url image

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -58,7 +58,7 @@ const Meta = (props: Meta) => {
             { name: 'twitter:title', content: title },
             { name: 'twitter:description', content: description },
             { name: 'twitter:creator', content: '@Nami' },
-            { name: 'twitter:image:src', content: image },
+            { name: 'twitter:image', content: image },
             { name: 'twitter:card', content: 'summary_large_image' },
             { name: 'og:title', content: title },
             { name: 'og:url', content: Config.env.APP_URL + url },
